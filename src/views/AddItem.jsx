6d4c6e050d0e076a0ffcfa4083fa35da45199e76
Patrick Snowden <patrickbsnowden@outlook.com>
2022-10-09 16:43:47 -0700

    added form to addItem component

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,87 @@
-export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+import { useState } from 'react';
+import { addItem } from '../api/firebase';
+
+export function AddItem({ listToken }) {
+	const [formData, setFormData] = useState({
+		itemName: '',
+		daysUntilNextPurchase: 0,
+	});
+
+	const [message, setMessage] = useState('');
+
+	const { itemName, daysUntilNextPurchase } = formData;
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await addItem(listToken, { itemName, daysUntilNextPurchase });
+			setFormData({
+				itemName: '',
+				daysUntilNextPurchase: 0,
+			});
+			setMessage('Item added');
+		} catch (error) {
+			console.log(error);
+			setMessage('Item not added');
+		}
+	};
+
+	const handleChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="itemName">Item Name:</label>
+					<input
+						type="text"
+						name="itemName"
+						id="itemName"
+						placeholder="item name"
+						value={itemName}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div>
+					<fieldset>
+						<legend>How soon will you buy this again?</legend>
+						<label htmlFor="soon">Soon</label>
+						<input
+							type="radio"
+							value={7}
+							name="daysUntilNextPurchase"
+							id="soon"
+							onChange={handleChange}
+						/>
+						<label htmlFor="kind-of-soon">Kind of soon</label>
+						<input
+							type="radio"
+							value={14}
+							name="daysUntilNextPurchase"
+							id="kind-of-soon"
+							onChange={handleChange}
+						/>
+						<label htmlFor="not-soon">Not soon</label>
+						<input
+							type="radio"
+							value={30}
+							name="daysUntilNextPurchase"
+							id="not-soon"
+							onChange={handleChange}
+						/>
+					</fieldset>
+					<p>{message}</p>
+				</div>
+				<div>
+					<button type="submit">Add Item</button>
+				</div>
+			</form>
+		</div>
+	);
 }
