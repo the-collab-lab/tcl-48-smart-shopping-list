c21131f1ca3876fbd71703b24ae30ed3145c7ca2
Patrick Snowden <96706605+snowsneakers@users.noreply.github.com>
2022-10-15 05:27:20 -0700

    added ability to add items to db (#19)
    * passed list token as props to add item component
    
    * imported addDoc method/ replaced console.log
    
    * added form to addItem component
    
    * added default of soon for add item form
    
    * changed checked to defaultChecked
    
    * removed setting days to 0 in submit function
    
    * if successful itemName gets cleared/ days remains
    
    * changed form style
    
    * added item name to the set message
    
    * corrected labels for radio buttons
    
    * corrected labels and id for radio inputs
    
    * added setTimeout to reset message("")
    
    * added finaly block to handleSubmit
    
    Co-authored-by: nuriya <nuriya_a@yahoo.com>

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,99 @@
-export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+import { useState } from 'react';
+import { addItem } from '../api/firebase';
+
+export function AddItem({ listToken }) {
+	const [formData, setFormData] = useState({
+		itemName: '',
+		daysUntilNextPurchase: 7,
+	});
+
+	const [message, setMessage] = useState('');
+
+	const { itemName, daysUntilNextPurchase } = formData;
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await addItem(listToken, { itemName, daysUntilNextPurchase });
+			setMessage(`${itemName} added to the list`);
+		} catch (error) {
+			console.log(error);
+			setMessage('Item not added');
+		} finally {
+			setTimeout(() => {
+				setFormData((prevState) => ({
+					...prevState,
+					itemName: '',
+				}));
+				setMessage('');
+			}, 2000);
+		}
+	};
+
+	const handleChange = (e) => {
+		if (message) setMessage('');
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="itemName">Item Name:</label>
+				</div>
+				<input
+					type="text"
+					name="itemName"
+					id="itemName"
+					placeholder="item name"
+					value={itemName}
+					onChange={handleChange}
+					required
+				/>
+				<div>
+					<fieldset>
+						<legend>How soon will you buy this again?</legend>
+						<div>
+							<input
+								type="radio"
+								value={7 || daysUntilNextPurchase}
+								name="daysUntilNextPurchase"
+								id="soon"
+								onChange={handleChange}
+								defaultChecked
+							/>
+							<label htmlFor="soon">Soon</label>
+						</div>
+						<div>
+							<input
+								type="radio"
+								value={14 || daysUntilNextPurchase}
+								name="daysUntilNextPurchase"
+								id="kind-of-soon"
+								onChange={handleChange}
+							/>
+							<label htmlFor="kind-of-soon">Kind of soon</label>
+						</div>
+						<div>
+							<input
+								type="radio"
+								value={30 || daysUntilNextPurchase}
+								name="daysUntilNextPurchase"
+								id="not-soon"
+								onChange={handleChange}
+							/>
+							<label htmlFor="not-soon">Not soon</label>
+						</div>
+					</fieldset>
+					<p>{message}</p>
+				</div>
+				<div>
+					<button type="submit">Add Item</button>
+				</div>
+			</form>
+		</div>
+	);
 }
