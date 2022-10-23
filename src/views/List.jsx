60ffae4398e67691d7df366f6557829da35ffdb9
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-10-23 19:49:29 -0400

    added button for adding items to empty list and fixed null bug in list that allowed users to add items to a null list

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,5 +1,6 @@
 import { ListItem } from '../components';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function List({ data }) {
 	const [searchQuery, setSearchQuery] = useState('');
@@ -11,29 +12,35 @@ export function List({ data }) {
 
 	return (
 		<>
-			<form className="filterForm">
-				<label htmlFor="searchItems">Filter Items</label>
-				<input
-					type="text"
-					id="searchItems"
-					name="searchItems"
-					value={searchQuery}
-					placeholder="Start typing here"
-					autoComplete="off"
-					onChange={(e) => setSearchQuery(e.target.value)}
-				/>
-				{searchQuery && <button onClick={clearInput}>Clear</button>}
-			</form>
-
-			<ul>
-				{data
-					.filter((item) =>
-						item.name.toLowerCase().includes(searchQuery.toLowerCase()),
-					)
-					.map((item) => {
-						return <ListItem key={item.id} name={item.name} />;
-					})}
-			</ul>
+			{data.length > 0 ? (
+				<ul>
+					<form className="filterForm">
+						<label htmlFor="searchItems">Filter Items</label>
+						<input
+							type="text"
+							id="searchItems"
+							name="searchItems"
+							value={searchQuery}
+							placeholder="Start typing here"
+							autoComplete="off"
+							onChange={(e) => setSearchQuery(e.target.value)}
+						/>
+						{searchQuery && <button onClick={clearInput}>Clear</button>}
+					</form>
+					{data
+						.filter((item) =>
+							item.name.toLowerCase().includes(searchQuery.toLowerCase()),
+						)
+						.map((item) => {
+							return <ListItem key={item.id} name={item.name} />;
+						})}
+				</ul>
+			) : (
+				<div>
+					<p>Your shopping list is currently empty.</p>
+					<Link to="/add-item">Add your first item!</Link>
+				</div>
+			)}
 		</>
 	);
 }
